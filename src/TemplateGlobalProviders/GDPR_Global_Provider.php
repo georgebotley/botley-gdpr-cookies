bbd7160358649b5ead8bb15bc500250a74593815
George Botley <george@botley.eu>
2020-03-12 11:15:07 +0000

    Update global provider variables

--- src/TemplateGlobalProviders/GDPR_Global_Provider.php
+++ src/TemplateGlobalProviders/GDPR_Global_Provider.php
@@ -15,17 +15,47 @@ class GDPR_Global_Provider implements TemplateGlobalProvider
     {
         return [
             'AllowedCookies',
+            'Cookie_StrictlyNecessary',
+            'Cookie_PerformanceTracking',
+            'Cookie_TargetingAdvertising',
         ];
     }
 
-
     /**
-     * Establish if Cookies are 
+     * Establish if Cookies are permitted
      *
-     * @return string
+     * @return boolean
      */
     public static function AllowedCookies()
     {
         return CookieController::cookieStatus();
     }
+    
+    /**
+     * Check if permission for StrictlyNeccessary cookies are provided.
+     *
+     * @return Boolean
+     */
+    public static function Cookie_StrictlyNecessary() {
+	    return CookieController::cookieStatus("Cookie_StrictlyNecessary");
+    }
+    
+    /**
+     * Check if permission for PerformanceTracking cookies are provided.
+     *
+     * @return Boolean
+     */
+    public static function Cookie_PerformanceTracking() {
+	    return CookieController::cookieStatus("Cookie_PerformanceTracking");
+    }
+    
+    /**
+     * Check if permission for TargetingAdvertising cookies are provided.
+     *
+     * @return Boolean
+     */
+    public static function Cookie_TargetingAdvertising() {
+	    return CookieController::cookieStatus("Cookie_TargetingAdvertising");
+    }
+    
 }
